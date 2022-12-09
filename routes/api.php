aea772a82ca8c1a8c07931222394a4b612ab2997
JalalMitali <JalalMitaliShopping@gmail.com>
2022-12-09 22:43:53 +0100

    stripe initial setup

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,34 @@ use App\Http\Controllers\ProductsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// routes/api.php
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user;
+})->middleware('auth:sanctum');
+
+Route::post('/purchase', function (Request $request) {
+    $stripeCharge = $request->user()->charge(
+        100, $request->paymentMethodId
+    );
+})->middleware('auth:sanctum');
+
+Route::post('/pay', function (Request $request) {
+    if (!$request->user()) {
+        return json_encode($request->user());
+    }
+    $payment = $request->user()->pay(
+        1400
+    );
+
+    return $payment->client_secret;
+})->middleware('auth:sanctum');
 
 Route::get('/products', [ProductsController::class, 'getAll']);
 Route::get('/products/{id}', [ProductsController::class, 'findOneByID']);
